4b6b4b54eeef0cd94744906035786c4159ac73a3
joychae <sooie35@gmail.com>
2021-08-01 21:35:58 +0900

    feat: 인메모리 사용자 스토어 추가

--- src/main/java/com/prac/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/prac/springsecurity/config/SecurityConfig.java
@@ -1,12 +1,32 @@
 package com.prac.springsecurity.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        UserDetails user = users
+                .username("joy")
+                .password("{noop}1234")
+                .roles("MEMBER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -18,5 +38,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // 세션 생성 정책 설정
         http.sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    // 인증 관리자를 스프링 빈으로 선언
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
